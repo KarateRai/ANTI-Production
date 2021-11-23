f95ccb03fd3be3964cbb06e85c576d86e53992f5
KarateRai <raimon420@gmail.com>
2021-11-23 14:35:39 +0100

    statsfix

--- Assets/Scripts/Units/Player/PlayerStats.cs
+++ Assets/Scripts/Units/Player/PlayerStats.cs
@@ -8,7 +8,12 @@ public class PlayerStats : UnitStats
     
     private int id_number;
 
-    public PlayerStats(PlayerController controller, int health, float speed) : base(controller, health, speed) { }
+    public PlayerStats(PlayerController controller, int health, float speed) : base(controller, health, speed) 
+    {
+        u_health = this._health;
+        u_speed = this.Speed;
+        Debug.Log("Uhealth / USpeed = " + u_health + "/" + u_speed + " in " + this.ToString());
+    }
 
     public int idNumber { get { return id_number; } set { id_number = value; } }
     public int Level { get { return level; } set { level = value; } }
