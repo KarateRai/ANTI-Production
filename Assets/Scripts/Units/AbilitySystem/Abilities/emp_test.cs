30706f68d0e0e97b7db311db4a322557cf0f5608
Whampaz <62597007+Whampaz@users.noreply.github.com>
2021-11-30 11:42:45 +0100

    ability_updates

--- Assets/Scripts/Units/AbilitySystem/Abilities/emp_test.cs
+++ Assets/Scripts/Units/AbilitySystem/Abilities/emp_test.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class emp_test : MonoBehaviour
 {
+    public float range;
+    public int damage;
+    public static Collider[] colliders;
     // Start is called before the first frame update
     void Start()
     {
-        
+        colliders = Physics.OverlapSphere(this.transform.position, range, 11);
+        foreach (Collider c in colliders)
+        {
+            if (c.tag == "AI")
+            {
+                c.GetComponent<EnemyController>().TakeDamage(damage);
+            }
+        }
     }
 
     // Update is called once per frame
