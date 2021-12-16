cf06d67ad9ae5abd017c2920a11b375717059d79
KarateRai <raimon420@gmail.com>
2021-12-16 10:07:32 +0100

    Weapon and testscene

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -8,7 +8,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller.SetShootingPos();
     }
 
     // Update is called once per frame
