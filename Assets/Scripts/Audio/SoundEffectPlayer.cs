fa0fe09262a0b1b4c1a3539b61dee26c276ab64c
Algelius <62651753+Algelius@users.noreply.github.com>
2022-01-10 11:44:12 +0100

    SoundEffectsPause

--- Assets/Scripts/Audio/SoundEffectPlayer.cs
+++ Assets/Scripts/Audio/SoundEffectPlayer.cs
@@ -8,13 +8,25 @@ public class SoundEffectPlayer : MonoBehaviour
 {
     private StudioEventEmitter emitter;
     private string filePath;
-
+    private bool playOnStart;
     private void Start()
     {
         emitter = GetComponent<StudioEventEmitter>();
         filePath = emitter.Event;
+        if (emitter.PlayEvent == EmitterGameEvent.ObjectStart) { playOnStart = true; }
     }
 
+    private void Update()
+    {
+        if (emitter.IsPlaying() && Time.timeScale == 0)
+        {
+            emitter.Stop();
+        }
+        else if (!emitter.IsPlaying() && Time.timeScale > 0 && playOnStart)
+        {
+            PlaySound();
+        }
+    }
     public void PlaySound()
     {
         if (emitter != null)
