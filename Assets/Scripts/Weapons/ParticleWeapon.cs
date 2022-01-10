67d9a6d2cf75ed911b2b040a0a0e900da3eba922
KarateRai <raimon420@gmail.com>
2022-01-10 15:06:08 +0100

    particle work

--- Assets/Scripts/Weapons/ParticleWeapon.cs
+++ Assets/Scripts/Weapons/ParticleWeapon.cs
@@ -30,7 +30,7 @@ public class ParticleWeapon : Weapon
         psMain = _particleProjectile.main;
         emitParams = new ParticleSystem.EmitParams();
     }
-    public override void Init(Transform shootingPos, LayerMask targetLayer, Gradient gradient)
+    public override void Init(Transform shootingPos, LayerMask targetLayer, Material m)
     {
         this.shootingPos = shootingPos;
         _particleProjectile = Instantiate(_particleProjectilePrefab);
@@ -40,8 +40,11 @@ public class ParticleWeapon : Weapon
         _particleProjectile.transform.rotation = shootingPos.rotation;
         var collision = _particleProjectile.collision;
         collision.collidesWith = targetLayer;
+        pL = _particleProjectile.GetComponent<ParticleLauncher>();
+        pL.Weapon = this;
         psMain = _particleProjectile.main;
-        psMain.startColor = gradient;
+        emitParams = new ParticleSystem.EmitParams();
+        psMain.startColor = new ParticleSystem.MinMaxGradient(m.color);
     }
 
     public override bool Fire()
